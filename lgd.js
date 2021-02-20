7ecce447de3fe20145413d88c7ffe560f84e3ef3
Max <62210396+mvpitr@users.noreply.github.com>
2021-02-20 10:47:03 -0500

    Update lgd.js

--- lgd.js
+++ lgd.js
@@ -3,7 +3,7 @@
   const Discord = require('discord.js');
   const client = new Discord.Client();
   const LIMIT = 20;
-  const MY_USER_ID = '219887155315474432';
+  const MY_USER_ID = 'USER-ID-HERE';
 
   var dict = {};
 
@@ -15,40 +15,12 @@
   });
 
 
-  /* client.on('message', (message) => { //whenever a message is sent
-    if (message.content.includes('tenor.com/') || message.content.includes('https://media.discordapp.net/attachments/696535474461736971/809618300233187338/strip.gif')) { //if it contains a link from tenor.com
-      var userId = message.author.id;
-      if(userId === '219887155315474432') {
-        message.delete() //delete the message
-        .then(message.author.send("Shut up!!!!"))
-      }
-    }
-  })
 
-  */
 
 
 
 
   client.on('message', (message) => { //whenever a message is sent
-   /* const exampleEmbed = new Discord.MessageEmbed()
-     .setColor('#0099ff')
-     .setTitle('Some title')
-     .setURL('https://discord.js.org/')
-     .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-     .setDescription('Some description here')
-     .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-    .addField('Inline field title', 'Some value here', true)
-    .setImage('https://i.imgur.com/wSTFkRM.png')
-    .setTimestamp()
-    .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-      
-    if (message.content === '!help') {
-      //client.channels.cache.find('539195804766830605').send(exampleEmbed);
-      message.reply(exampleEmbed);
-    }
-  */
-
     var userId = message.author.id;
     console.log(`User identified as ${userId}.`);
     if(userId === MY_USER_ID) {
@@ -109,4 +81,4 @@
 
 
 
-  client.login('ODEyMzYzNDI5NzEyNjI1Njk2.YC_qZg.e1aY_ftZ4Q22OiCiOY0ycg6PRgo');
+  client.login('YOUR-TOKEN-HERE');
